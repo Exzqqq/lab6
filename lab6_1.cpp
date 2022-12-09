48e69ac3d86628903a9d343f14b0c6a423124163
pun <pariwat_wong@cmu.ac.th>
2022-12-09 17:05:12 +0700

    lab6_2.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,29 @@
 using namespace std;
 
 int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    int i;
+    int n;
+    int odd=0;
+    int even=0;
+
+   do
+   {
+      cout << "Enter an integer: ";
+    cin >>n;
+
+    if (n%2==0&&n!=0)
+    {
+       even++;
+    }else if (n%2!=0&&n!=0)
+    {
+        odd++;
+    }
+   
+    
+   } while (n!=0);
+   
+
+    cout << "#Even numbers = "<<even<<endl;
+    cout << "#Odd numbers = "<<odd;
     return 0;
 }
